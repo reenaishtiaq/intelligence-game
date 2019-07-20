f8b922f1f443e953eadae7812d429b00e574cadd
reenaishtiaq <49521474+reenaishtiaq@users.noreply.github.com>
2019-07-20 17:22:18 +0500

    WIP

--- script.js
+++ script.js
@@ -1,10 +1,216 @@
-function intelligence() {
-    let int = "you r Einstein"
 
-    let name = document.querySelector("#talent").value;
+let intelligenceLevel = [
+    "good",
+    "smart",
+    "fine",
+    "extra ordinary",
+    "cool",
+    "awesome"
+]
 
-    document.querySelector("#talent").value;
+function showTalent() {
+    let name = document.querySelector("input").value;
 
-    document.querySelector("p").innerHTML = name + ", " + int;
+    if (name == false) {
+        alert("input your name");
 
+    } else {
+        let x = parseInt(Math.random() * 6);
+        document.querySelector("p").innerHTML = "you are " + intelligenceLevel[x];
+
+
+
+
+    }
 }
+
+
+
+    // let intelligence;
+
+    // if (name == false) {
+    //     alert("plaese enter a name");
+
+    // } else {
+    //     if (name.charAt(0).tolowerCase() == "a") {
+    //         intelligence = intelligenceLevel[6];
+    //         alert(intelligence);
+
+    //     } else if (name.charAt(0).tolowerCase() == "b") {
+    //         intelligence = intelligenceLevel[4];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "c") {
+    //         intelligence = intelligenceLevel[1];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "d") {
+    //         intelligence = intelligenceLevel[5];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "e") {
+    //         intelligence = intelligenceLevel[2];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "f") {
+    //         intelligence = intelligenceLevel[4];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "g") {
+    //         intelligence = intelligenceLevel[46];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "h") {
+    //         intelligence = intelligenceLevel[3];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "i") {
+    //         intelligence = intelligenceLevel[2];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "j") {
+    //         intelligence = intelligenceLevel[1];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "k") {
+    //         intelligence = intelligenceLevel[3];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "l") {
+    //         intelligence = intelligenceLevel[4];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "m") {
+    //         intelligence = intelligenceLevel[3];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "n") {
+    //         intelligence = intelligenceLevel[3];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "o") {
+    //         intelligence = intelligenceLevel[1];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "p") {
+    //         intelligence = intelligenceLevel[5];
+
+    //         alert(intelligence);
+
+
+    //     }
+    //     else if (name.charAt(0).tolowerCase() == "q") {
+    //         intelligence = intelligenceLevel[1];
+
+    //         alert(intelligence);
+
+
+//         }
+//         else if (name.charAt(0).tolowerCase() == "r") {
+//             intelligence = intelligenceLevel[4];
+
+//             alert(intelligence);
+
+
+//         }
+//         else if (name.charAt(0).tolowerCase() == "s") {
+//             intelligence = intelligenceLevel[3];
+
+//             alert(intelligence);
+
+
+//         }
+//         else if (name.charAt(0).tolowerCase() == "t") {
+//             intelligence = intelligenceLevel[1];
+
+//             alert(intelligence);
+
+
+//         }
+//         else if (name.charAt(0).tolowerCase() == "u") {
+//             intelligence = intelligenceLevel[4];
+
+//             alert(intelligence);
+
+
+//         }
+//         else if (name.charAt(0).tolowerCase() == "v") {
+//             intelligence = intelligenceLevel[1];
+
+//             alert(intelligence);
+
+
+//         }
+//         else if (name.charAt(0).tolowerCase() == "w") {
+//             intelligence = intelligenceLevel[2];
+
+//             alert(intelligence);
+
+
+//         }
+//         else if (name.charAt(0).tolowerCase() == "x") {
+//             intelligence = intelligenceLevel[6];
+
+//             alert(intelligence);
+
+
+//         }
+//         else if (name.charAt(0).tolowerCase() == "y") {
+//             intelligence = intelligenceLevel[3];
+
+//             alert(intelligence);
+
+
+//         }
+//         else if (name.charAt(0).tolowerCase() == "z") {
+//             intelligence = intelligenceLevel[5];
+
+//             alert(intelligence);
+
+
+//         }
+//     }
+
+// }
