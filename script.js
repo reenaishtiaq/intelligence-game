bb68d0fb4a78c7696b17cd985b7ddc1b68798143
reenaishtiaq <49521474+reenaishtiaq@users.noreply.github.com>
2019-07-20 12:33:50 +0500

    WIP

---
+++ script.js
@@ -0,0 +1,10 @@
+function intelligence() {
+    let int = "you r Einstein"
+
+    let name = document.querySelector("#talent").value;
+
+    document.querySelector("#talent").value;
+
+    document.querySelector("p").innerHTML = name + ", " + int;
+
+}
